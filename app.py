42c0309c47abe90815f246d6f311c0f1af29eddb
Salvador Montiel <salvadormontiel8@gmail.com>
2021-10-31 20:34:37 -0600

    Update app

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_app():
 def main():
     """Main entrypoint."""
     app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=80)
 
 
 if __name__ == "__main__":
