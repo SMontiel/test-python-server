68c8a6a7c382acb1e97f5fb092f749408bd8c324
Salvador Montiel <salvadormontiel8@gmail.com>
2021-10-31 22:48:32 -0600

    Update app

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def create_app():
     @app.route('/')
     def index():
         """Example route."""
-        message = f"Hello from {socket.gethostname()}. Salvador is here! Loviu, cosi <3"
-        return {"message": "Indio oaxaquenio .l."}, 200
+        message = f"Hello from {socket.gethostname()}. Salvador is here! "
+        return { "message": message }, 200
 
     return app
 
